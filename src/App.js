2c568ec0987df5343a56fc612b2834c9c3482da2
majusoler <juliasolernunes@ymail.com>
2022-10-11 15:46:47 +0200

    Use effect in use

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useEffect, useState } from "react";
+
+import axios from "axios";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [drinks, setDrinks] = useState(null);
+
+  async function getDrinks() {
+    const response = await axios.get(
+      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka"
+    );
+    setDrinks(response.data.drinks);
+  }
+
+  useEffect(() => {
+    getDrinks();
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(counter);
+  // }, [counter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Have some drinks!</h1>
+      <ul>
+        {drinks ? (
+          drinks.map((drink) => {
+            return <li key={drink.idDrink}>{drink.strDrink}</li>;
+          })
+        ) : (
+          <div>Loading</div>
+        )}
+      </ul>
+
+      {/* <h3>{counter}</h3>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Click me to have fun!
+      </button> */}
     </div>
   );
 }
